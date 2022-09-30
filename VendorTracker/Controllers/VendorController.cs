8ccbbc6fb9e8ce489d9f24470d864f34a9804a92
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-30 13:48:59 -0700

    vendor controller

--- VendorTracker/Controllers/VendorController.cs
+++ VendorTracker/Controllers/VendorController.cs
@@ -28,32 +28,32 @@ namespace VendorTracker.Controllers
       return RedirectToAction("Index");
     }
 
-    [HttpGet("/vendors/{id}")]
-    public ActionResult Show(int id)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor selectedVendor = Vendor.Find(id);
-      List<Order> vendorOrders = selectedvendor.Orders;
-      model.Add("vendor", selectedVendor);
-      model.Add("order", vednorOrders);
-      return View(model);
-    }
+    // [HttpGet("/vendors/{id}")]
+    // public ActionResult Show(int id)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Vendor selectedVendor = Vendor.Find(id);
+    //   List<Order> vendorOrders = selectedvendor.Orders;
+    //   model.Add("vendor", selectedVendor);
+    //   model.Add("order", vednorOrders);
+    //   return View(model);
+    // }
 
 
-    // This one creates new Orders within a given vednor, not new vendors:
+    // // This one creates new Orders within a given vednor, not new vendors:
 
-    [HttpPost("/vendors/{vednorId}/order")]
-    public ActionResult Create(int vednorId, string orderDescription)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Item(itemDescription);
-      foundVendor.AddItem(newOrder);
-      List<Item> vendorItems = foundVendor.Items;
-      model.Add("order", vendorItems);
-      model.Add("vendor", foundVendor);
-      return View("Show", model);
-    }
+    // [HttpPost("/vendors/{vednorId}/order")]
+    // public ActionResult Create(int vednorId, string orderDescription)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Vendor foundVendor = Vendor.Find(vendorId);
+    //   Order newOrder = new Item(itemDescription);
+    //   foundVendor.AddItem(newOrder);
+    //   List<Item> vendorItems = foundVendor.Items;
+    //   model.Add("order", vendorItems);
+    //   model.Add("vendor", foundVendor);
+    //   return View("Show", model);
+    // }
 
   }
 }
