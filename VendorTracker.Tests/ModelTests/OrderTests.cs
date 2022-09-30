754bcfc1883786fb0add42308a8ae1e491bf8de2
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-30 12:25:23 -0700

    order tests passing

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -1,24 +1,84 @@
-// using System;
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using System.Collections.Generic;
-// using Vendor.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorTracker.Models;
+using System;
 
-// namespace Vendor.Tests
-// {
-//   [TestClass]
-//   public class OrderTests : IDisposable
-//   {
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
 
-//     public void Dispose()
-//     {
-//       Order.ClearAll();
-//     }
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
 
-//     [TestMethod]
-//     public void OrderConstructor_CreatesInstanceOrder_Order()
-//     {
-//       Order newOrder = new Order("test");
-//       Assert.AreEqual(typeof(Order), newOrder.GetType());
-//     }
-//   }
-// }
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "this is the order description";
+
+      //Act
+      Order newOrder = new Order(description);
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "this is the order description";
+      Order newOrder = new Order(description);
+
+      //Act
+      string updatedDescription = "order for Broken Robot";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      // Arrange
+      List<Order> newList = new List<Order> { };
+
+      // Act
+      List<Order> result = Order.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      //Arrange
+      string description01 = "cafe";
+      string description02 = "grocery store";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      //Act
+      List<Order> result = Order.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+  }
+}
