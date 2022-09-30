fe5598aac4d21a9e7acf0842b2e68554bf3a0434
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-30 09:18:13 -0700

    add boiler plate and first test

---
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Vendor.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
