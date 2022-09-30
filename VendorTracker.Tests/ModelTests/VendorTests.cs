c5bad9c7a3fa44c52a179031b5e866772ea2bb73
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-30 10:11:17 -0700

    first test passing

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -3,22 +3,39 @@ using VendorTracker.Models;
 using System.Collections.Generic;
 using System;
 
-namespace Vendor.Tests
+namespace VendorTracker.Tests
 {
-//   [TestClass]
-//   public class VendorTests : IDisposable
-//   {
+  [TestClass]
+  public class VendorTests //: IDisposable
+  {
 
-//     public void Dispose()
-//     {
-//       Vendor.ClearAll();
-//     }
+    // public void Dispose()
+    // {
+    //   Vendor.ClearAll();
+    // }
 
-    [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
-    {
-      Vendor newVendor = new Vendor("test vendor");
-      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-    }
+  [TestMethod]
+  public void GetName_ReturnsName_String()
+  {
+    //Arrange
+    string name = "Test Vendor";
+    Vendor newVendor = new Vendor(name);
+
+    //Act
+    string result = newVendor.Name;
+
+    //Assert
+    Assert.AreEqual(name, result);
+  }
+
+
+
+    // [TestMethod]
+    // public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    // {
+    //   Vendor newVendor = new Vendor("test vendor");
+    //   Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    // }
 //   }
+  }
 }
